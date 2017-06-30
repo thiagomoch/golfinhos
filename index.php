8cd2befe3ae56dfae2b393e67458220dc6525176
Thiago Nunes <thiago@webinterativa.com.br>
2017-06-30 10:14:59 -0300

    ajustes

---
+++ index.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<!--[if lt IE 7 ]> <html lang="pt-BR" class="no-js oldie ie6 lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7 ]>    <html lang="pt-BR" class="no-js oldie ie7 lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8 ]>    <html lang="pt-BR" class="no-js oldie ie8 lt-ie9"> <![endif]-->
+<!--[if IE 9 ]>    <html lang="pt-BR" class="no-js ie9"> <![endif]-->
+<!--[if (gt IE 9)|!(IE)]><!--><html lang="pt-BR" class="no-js" ><!--<![endif]-->
+<head>
+	<meta charset="utf-8">
+	<title>Golfinhos Fitness</title>
+	<meta name="viewport" content="width=device-width">
+    <meta name="description" content="">
+</head>
+<body>
+<!--[if lt IE 8]>
+    <p class="chromeframe">Você está usando um navegador <strong>antigo</strong>. Por favor, <a href="http://browsehappy.com/">atualize o seu navegador</a> ou <a href="http://www.google.com/chromeframe/?redirect=true">ative o Google Chrome Frame</a> para melhorar a sua experiência na web.</p>
+<![endif]-->
+<div class="wrapper" style="text-align:center;">
+	<img src="SITE.png" alt="">
+</div><!--/.wrapper -->
+
+</body>
+</html>
